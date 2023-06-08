bde0646aa67fef488d4f7f0672caa754af380159
준하 노 <tnptt1122@gmail.com>
2023-06-08 12:30:22 +0900

    feat: close 메서드 및 connected 시그널 추가
    - v 1.0.2

--- deviceAPIs/spectrometer.py
+++ deviceAPIs/spectrometer.py
@@ -4,11 +4,16 @@ from PySide6.QtCore import QThread, QTimer, Signal, Slot
 
 
 class Spectrometer(QThread):
+    isConnected = False
+
+    limitTop = None
+    limitBottom = None
+
     connectedSignal = Signal(bool)
     infoSignal = Signal(list)
     ramanSignal = Signal(list)
 
-    def __init__(self, signalInterval=1000, isVirtual=False):
+    def __init__(self, isVirtual=False, signalInterval=1000):
         super().__init__()
         try:
             self.spec = st.Spectrometer.from_first_available() if isVirtual else sb.Spectrometer.from_first_available()
@@ -16,17 +21,25 @@ class Spectrometer(QThread):
             self.timer = QTimer()
             self.timer.timeout.connect(self.emitInfoSignal)
             self.timer.start(signalInterval)
+            self.isConnected = True
             self.connectedSignal.emit(True)
 
         except Exception as e:
             print("스펙트로미터 장치에 연결할 수 없습니다.", e)
             self.connectedSignal.emit(False)
 
+    def close(self):
+        self.spec.close()
+
     def setIntegrationTime(self, value):
         self.spec.integration_time_micros(value)
 
     def getSpectrum(self):
         return self.spec.spectrum()
+
+    @Slot()
+    def checkConnected(self):
+        self.connectedSignal.emit(self.isConnected)
 
     @Slot()
     def emitInfoSignal(self):
@@ -37,3 +50,4 @@ class Spectrometer(QThread):
     def getRamanShift(self, laserWavelength):
         ramanShift = (1 / laserWavelength - 1 / self.getSpectrum()[0]) * (10 ** 7)
         self.ramanSignal.emit(ramanShift)
+        return ramanShift
